ca1cc3d092e2d53609bdf4d9bac54517a9ee6003
Kdoggg666 <kenanwright@gmail.com>
2021-06-01 15:55:30 +0000

    Added checkout views and forms

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'products',  # custom products app
     'bag',  # custom shopping cart app
     'checkout',  # Custom checkout app
+    'crispy_forms',  # Crispy forms lets you format forms using bootstrap automatically
 
 ]
 
@@ -61,6 +62,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -75,8 +78,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # Required by all auth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
