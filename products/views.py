e15855945fb62d93cdd7ccc90af4f67f29e8c85d
Kdoggg666 <kenanwright@gmail.com>
2021-06-28 10:37:22 +0000

    Finished add product functionailty

--- products/views.py
+++ products/views.py
@@ -73,11 +73,20 @@ def add_product(request):
     """
     add product to store
     """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Succesfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else: 
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
 
     return render(request, template, context)
-    
--- profiles/views.py
+++ profiles/views.py
@@ -17,8 +17,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     template = 'profiles/profile.html'
     context = {
@@ -33,7 +35,7 @@ def profile(request):
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
-    messages.infor(request, (
+    messages.info(request, (
         f'This is a past confirmation for order number {order_number}.'
         'A confirmation email was sent on the order date.'
         ))
