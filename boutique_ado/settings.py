1d79b43b61a6b41a9f9eba8c5d079da13c5613a4
Kdoggg666 <kenanwright@gmail.com>
2021-07-01 16:23:18 +0000

    Added secret key to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8+u1&i9)0n47$m^1&tha5k66e*t+9zqunu-lvggn&fbsz!b%v@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['online-store-walkthrough.herokuapp.com', 'localhost']
 
